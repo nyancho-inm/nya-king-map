88505ab2b3d8ff4be6f2a607127310e507c200f3
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-14 18:38:11 +0900

    画像投稿機能実装

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -2,13 +2,20 @@ class CatsController < ApplicationController
 before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    
+    @cats = Cat.all
   end
 
   def new
     @cat = Cat.new
   end
 
+  def create
+    Cat.create(cat_params)
+  end
+
   private
 
+  def cat_params
+    params.require(:cat).permit(:message, :prefecture_id, :area, :place, :image).merge(user_id: current_user.id)
+  end
 end
