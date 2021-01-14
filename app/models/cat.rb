abfcc7944d82e69c53453992b9a459c3b68b73e5
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-14 19:00:22 +0900

    ActiveHashで都道府県を表示

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,9 +1,14 @@
 class Cat < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to :user
+  has_one_attached :image
+  
   with_options presence: true do
     validates :prefecture_id
     validates :area
   end
+  validates :prefecture_id, numericality: { other_than: 0 }
 
-  belongs_to :user
-  has_one_attached :image
+  
 end
