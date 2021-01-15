63cad8918895694727891bc467a02a91a4d0fa9b
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-15 14:41:04 +0900

    コントローラー設定

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -22,6 +22,19 @@ before_action :authenticate_user!, except: [:index, :show]
     @cat = Cat.find(params[:id])
   end
 
+  def edit
+    @cat = Cat.find(params[:id])
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update(cat_params)
+      redirect_to cat_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def cat_params
