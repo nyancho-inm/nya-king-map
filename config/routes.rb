8e6606c61fcb9d6a69c8eff08387329b22026db5
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-16 13:05:12 +0900

    ルーティング、マイグレーション実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'comments/new'
   devise_for :users
   root to: 'cats#index'
-  resources :cats
+  resources :cats do
+    resources :comments, only: [:create]
+  end
 end
