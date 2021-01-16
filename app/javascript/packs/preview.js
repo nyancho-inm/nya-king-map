61b20188cfe807f15fe13b292fd2879ba7f648b3
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-16 11:43:46 +0900

    JSファイルを設定

--- app/javascript/packs/preview.js
+++ app/javascript/packs/preview.js
@@ -1,25 +1,37 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
-  document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
-
+  document.addEventListener('DOMContentLoaded', function() {
+    const ImageList = document.getElementById('image-list')
     const createImageHTML = (blob) => {
-      const imageElement = document.createElement('div');
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    };
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('class', "image-element")
+      let imageElementNum = document.querySelectorAll('.image-element').length
 
-    document.getElementById('cat-image').addEventListener('change', function(e){
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `cat-image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'cat[images][]')
+      inputHTML.setAttribute('type', 'file')
 
-      createImageHTML(blob);
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
+
+    document.getElementById('cat-image').addEventListener('change', (e) => {
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob)
+
     });
   });
 }
