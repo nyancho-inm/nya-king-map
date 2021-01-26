ece9a988a019c4c94e0bd44dd610f470f9311246
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-26 19:55:40 +0900

    ゲストログイン再追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  devise_for :users
-
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: 'cats#index'
   resources :cats do
     resources :likes, only: [:create, :destroy]
@@ -10,6 +13,7 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+  get 'comments/new'
   post 'like/:id' => 'likes#create', as: 'create_like'
   delete 'like/:id' => 'likes#destroy', as: 'destroy_like'
   get '/cat/prefecture', to: "cats#prefecture"
