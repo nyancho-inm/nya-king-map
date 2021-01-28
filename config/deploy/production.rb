65096841a3719353713b7e2ac970303b6ccf20e4
nyancho-inm <75053805+nyancho-inm@users.noreply.github.com>
2021-01-28 14:31:17 +0900

    production.rb修正

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '18.181.149.109', user: 'ec2-user', roles: %w{app db web}
+# server '18.181.149.109', user: 'ec2-user', roles: %w{app db web}
